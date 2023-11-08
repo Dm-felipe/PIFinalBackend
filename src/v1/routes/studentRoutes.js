ba6ce84a4699aed504f94d44706d08960519929a
Dm_felipe <40633889+Dm-felipe@users.noreply.github.com>
2023-11-08 17:15:11 -0400

    Update studentRoutes.js

--- src/v1/routes/studentRoutes.js
+++ src/v1/routes/studentRoutes.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const studentController = require('../../controllers/studentController');
+const requireAuthentication = require('./requireAuthentication'); // Importa tu middleware de autenticación
 
 const router = express.Router();
 
-router
-    .get('/', studentController.getAllStudents)
-    .get('/:id', studentController.getStudentById)
-    .post('/', studentController.createStudent)
-    .patch('/:id', studentController.updateStudent)
-    .delete('/:id', studentController.deleteStudent);
+router.get('/', studentController.getAllStudents);
+router.get('/:id', studentController.getStudentById);
+
+// middleware de autenticación solo a estas rutas
+router.use(requireAuthentication);
+
+router.post('/', studentController.createStudent);
+router.patch('/:id', studentController.updateStudent);
+router.delete('/:id', studentController.deleteStudent);
 
 module.exports = router;
